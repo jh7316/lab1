f5c664afff841b787279bc536acc4b39d5db192f
Chloe Han <jh7316@snappy1.cims.nyu.edu>
2022-09-25 06:12:40 -0400

    saving my changes

--- array.c
+++ array.c
@@ -13,6 +13,12 @@
 int array_sum(int *arr, int n)
 {
   // TODO: Your code here.
+  int i=0; int sum=0;
+  for(i=0;i<n;i++){
+    sum+= *(arr+i);
+  }
+  
+  return sum;
 }
 
 // Copy the first n elements of array "src" into array "dst".
@@ -21,7 +27,10 @@ int array_sum(int *arr, int n)
 void array_cpy(int *dst, int *src, int n)
 {
   // TODO: Your code here.
-
+  int i=0;
+  for(i=0;i<n;i++){
+    dst[i]=src[i];
+  }
 }
 
 // bubble_sort implements the bubble sort algorithm.
@@ -34,6 +43,16 @@ void array_cpy(int *dst, int *src, int n)
 void bubble_sort(int *arr, int n)
 {
   // TODO: Your code here.
+  int i,j;
+  for(i=0;i<n-1;i++){
+    //the last i integers are sorted
+    for(j=0;j<n-1-i;j++){
+      if(arr[j]>arr[j+1]){
+        //if the former element is greater than the latter one, swap them 
+        swap_int(&arr[j], &arr[j+1]);
+      }
+    }
+  }
 }
 
 //arr is a 4 byte array containing an integer in big endian format,
@@ -42,6 +61,8 @@ void bubble_sort(int *arr, int n)
 int big_to_little_endian(char *arr)
 {
   // TODO: Your code here.
-
+  //sending arr[3] to arr[0], arr[2] to arr[1], arr[1] to arr[2], arr[0] to arr[3] and combining them using OR operator into one integer value
+  int result= ((arr[3] & 0xFF)<<24)|((arr[2] & 0xFF)<<16)|((arr[1] & 0xFF)<<8)|(arr[0] & 0xFF);
+  return result;
 }
 
--- bitfloat.c
+++ bitfloat.c
@@ -1,12 +1,17 @@
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
 
 // Return the bit value (0 or 1) at position pos of unsigned int n
 // Note that the least significant bit position is 0. 
 int get_bit_at_pos(unsigned int n, int pos)
 {
   // TODO: Your code here.
+  int value=(n>>pos)&1;
+  return value;
 }
 
 // Flip the bit value (1->0 or 0->1) at position pos of unsigned int n,
@@ -16,19 +21,37 @@ int get_bit_at_pos(unsigned int n, int pos)
 int flip_bit_at_pos(unsigned int n, int pos)
 {
   // TODO: Your code here.
+    int a = 1<<pos;
+    //XOR operator with the integer a results in flipping the bit of given position
+    return (n^a);
+  	
 }
 
 // Return the most significant byte of unsigned int n
 char get_most_significant_byte(unsigned int n)
 {
   // TODO: Your code here.
-
+ //getting the left-most byte of the number
+ char x = (n>>24)&0xFF;
+ return x;
 }
 
 // Return true if n1+n2 causes overflow, return false otherwise
 bool sum_overflowed(int n1, int n2)
 {
   // TODO: Your code here.
+ if (n1>=0 && n2>=0) {
+  //when both integers are positive & they add up to a number higher than the maximum int value, overflow occurs
+  if (INT_MAX-n1<=n2) {
+   return true;
+  }
+ }else if(n1<0 && n2<0){
+  ////when both integers are negative & they add up to a number lower than the minimum int value, overflow occurs
+  if (n1<=INT_MIN-n2) {
+   return true;
+  }
+ }
+ return false;
 }
 
 //Extract the 8-bit exponent field of single precision floating point number f 
@@ -36,6 +59,14 @@ bool sum_overflowed(int n1, int n2)
 unsigned char get_exponent_field(float f) 
 {
 	//TODO: Your code here.
+	unsigned int* r = (unsigned int*) &f;
+	
+	//left-shift by one in order to remove the sign
+	*r=*r<<1;
+	
+	//return the third byte from the memory storage
+	return *((unsigned char*)r + 3);
+
 }
 
 //Bonus problem: Return the precision of floating point number f
@@ -44,4 +75,5 @@ unsigned char get_exponent_field(float f)
 float get_precision(float f)
 {
   //TODO: Your code here
+	
 }
